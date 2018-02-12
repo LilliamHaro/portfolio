682b5085711f6cf4f976b8ece78535ddad1d5312
LilliamHaro <qualia950@gmail.com>
2018-02-12 00:08:49 -0500

    subiendo avances

---
+++ js/app.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  var scroll = function() {
+    if ($(document).scrollTop() > 120) {
+      $('.navbar-fixed-top').addClass('bg-black');
+      // $('.navbar-fixed-top').removeClass('height-10');
+    } else {
+      $('.navbar-fixed-top').removeClass('bg-black');
+      // $('.navbar-fixed-top').addClass('height-10');
+    }
+  }
+  scroll();
+  $(window).scroll(function() {
+    scroll();
+  });
+});
