637635f8cf8f6096361725fc38b029fa4b3ba716
LilliamHaro <qualia950@gmail.com>
2018-02-15 11:21:05 -0500

    scrool en altura de navbar

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
   var scroll = function() {
     if ($(document).scrollTop() > 120) {
-      $('.navbar-fixed-top').addClass('bg-black');
+      $('.navbar-fixed-top').addClass('bg-nav-scroll');
       // $('.navbar-fixed-top').removeClass('height-10');
     } else {
-      $('.navbar-fixed-top').removeClass('bg-black');
+      $('.navbar-fixed-top').removeClass('bg-nav-scroll');
       // $('.navbar-fixed-top').addClass('height-10');
     }
   };
